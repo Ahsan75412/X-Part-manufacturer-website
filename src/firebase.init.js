4ed6a62fa4f842563bce5eb137dfdd7c8c7d114c
Ahsan Habib <ahsanpub16b@gmail.com>
2022-05-29 00:24:03 +0600

    added here env local file and update firebase init js

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,13 +6,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6Yd638QtMAxgckFT1v9979lTdCQf3nvw",
-  authDomain: "x-part.firebaseapp.com",
-  projectId: "x-part",
-  storageBucket: "x-part.appspot.com",
-  messagingSenderId: "9515352371",
-  appId: "1:9515352371:web:541a7a50a206b14c4bfe46"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
